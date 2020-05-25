ad0ce74d573ffbdb6ccb160ff5566ac1c45129f2
christian kinate <kinate.christian1989@yahoo.com>
2020-05-26 00:53:42 +0100

    Initial Commit

---
+++ DatingApp.API/Migrations/20200525122300_initialCreate0.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class initialCreate0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Values",
+                table: "Values");
+
+            migrationBuilder.RenameTable(
+                name: "Values",
+                newName: "ValuesTb");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ValuesTb",
+                table: "ValuesTb",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ValuesTb",
+                table: "ValuesTb");
+
+            migrationBuilder.RenameTable(
+                name: "ValuesTb",
+                newName: "Values");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "Id");
+        }
+    }
+}
